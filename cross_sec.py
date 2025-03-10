37526bbbee5d0095d0769000957432c62726f60a
samderegt <regt@strw.leidenuniv.nl>
2025-03-10 15:35:56 +0100

    Changed to fixed, higher-resolution wavenumber-spacing for super-line treatment

--- cross_sec.py
+++ cross_sec.py
@@ -25,7 +25,8 @@ class CrossSection:
         # Create wavenumber grid
         self._set_nu_grid(conf)
         self.adaptive_nu_grid = getattr(conf, 'adaptive_nu_grid', False)
-        self.resolution_local_cutoff = getattr(conf, 'resolution_local_cutoff', 1e6) # Same as ExoMol
+        # Finer resolution to determine which lines to keep
+        self.delta_nu_local_cutoff = getattr(conf, 'delta_nu_local_cutoff', 1e-3) # [cm^-1]
         
         # (P,T)-grid to compute cross-sections on
         self.P_grid = np.asarray(conf.P_grid) * 1e5 # [bar] -> [Pa]
@@ -211,8 +212,7 @@ class CrossSection:
     def _local_cutoff(self, S, nu_0, factor):
 
         # Round to zero-th decimal
-        delta_nu = nu_0 / self.resolution_local_cutoff
-        nu_bin = np.around((nu_0-self.nu_min)/delta_nu).astype(int)
+        nu_bin = np.around((nu_0-self.nu_min)/self.delta_nu_local_cutoff).astype(int)
 
         # Upper and lower indices of lines within bins
         _, nu_bin_idx = np.unique(nu_bin, return_index=True)
