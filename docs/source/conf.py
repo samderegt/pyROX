826b8447d0456fb7216db141cd9f6ee1afae9b93
samderegt <regt@strw.leidenuniv.nl>
2025-04-07 14:28:17 +0200

    Removed build from docs and added functionality for notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,14 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['**.ipynb_checkpoints']
 
 
 
